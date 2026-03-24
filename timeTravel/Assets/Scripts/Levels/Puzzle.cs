97079f1a0d440ea2ad0dc326b28de9224fcf80a2
30JordanC <carvalhojordan56@gmail.com>
2026-03-23 23:35:36 -0400

    Fixed my puzzle math
    I fixed it where it would show up when player collides with the cube(will be key in the future) showing a simple math problem. when the math. When solved correctly it will close the pannel. when not solved correctly it will do nothing.

--- timeTravel/Assets/Scripts/Levels/Puzzle.cs
+++ timeTravel/Assets/Scripts/Levels/Puzzle.cs
@@ -1,9 +1,11 @@
 using UnityEngine;
-
+using TMPro;
 public class Puzzle : MonoBehaviour
 {
     public bool isSolved = false;
     public GameObject puzzleUI;
+    public TMP_InputField answerInput;
+    public int correctAnswer = 8;
 
     public void SolvePuzzle()
     {
@@ -15,6 +17,8 @@ public class Puzzle : MonoBehaviour
         {
             level.OnPuzzleSolved();
         }
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
 
         puzzleUI.SetActive(false);
     }
@@ -27,6 +31,29 @@ public class Puzzle : MonoBehaviour
 
             gameObject.SetActive(false);
             puzzleUI.SetActive(true);
+            //unlocking cursor 
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+        }
+    }
+    public void CheckAnswer()
+    {
+        int playerAnswer;
+
+        if (int.TryParse(answerInput.text, out playerAnswer))
+        {
+            if (playerAnswer == correctAnswer)
+            {
+                SolvePuzzle();
+            }
+            else
+            {
+                Debug.Log("Wrong answer!");
+            }
+        }
+        else
+        {
+            Debug.Log("Invalid input!");
         }
     }
 }
