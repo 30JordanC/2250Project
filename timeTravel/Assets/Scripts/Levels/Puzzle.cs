1da6efb929e0473ff614d24cecc41f0019aafb52
30JordanC <carvalhojordan56@gmail.com>
2026-03-23 22:48:44 -0400

    updated the look of level 2 adding a starting area for it, fixed the parkour section of the level so that the player can make all the jumps, updated my puzzle code so that when the player touches the cube the cube will go away and it will start the pop up window for the math puzzle to open the final door to the staff

--- timeTravel/Assets/Scripts/Levels/Puzzle.cs
+++ timeTravel/Assets/Scripts/Levels/Puzzle.cs
@@ -3,13 +3,30 @@ using UnityEngine;
 public class Puzzle : MonoBehaviour
 {
     public bool isSolved = false;
+    public GameObject puzzleUI;
 
     public void SolvePuzzle()
     {
         isSolved = true;
         Debug.Log("Puzzle solved!");
 
-        // Notify level
-        FindObjectOfType<EgyptLevel>().OnPuzzleSolved();
+        EgyptLevel level = FindObjectOfType<EgyptLevel>();
+        if (level != null)
+        {
+            level.OnPuzzleSolved();
+        }
+
+        puzzleUI.SetActive(false);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("Player touched the cube!");
+
+            gameObject.SetActive(false);
+            puzzleUI.SetActive(true);
+        }
     }
 }
