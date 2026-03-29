8c7f86967d9616e03883bc9c86cf4556af6d07d4
30JordanC <carvalhojordan56@gmail.com>
2026-03-29 18:16:25 -0400

    Checkpoints
    added checkpoints to my level so that when the player walks over them, that is there new checkpoint and when they die they respawned there

---
+++ timeTravel/Assets/Scripts/Levels/Checkpoint.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class Checkpoint : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            RespawnManager.instance.SetCheckpoint(transform);
+            Debug.Log("Checkpoint activated!");
+        }
+    }
+}
