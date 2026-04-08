723989efe783d4c02059f57e0d2c7f85c432e904
sunakshirn <srana69@uwo.ca>
2026-04-08 13:41:23 -0400

    Revert "Merge branch 'Suna1' into MainAttempt3"
    This reverts commit 3c39f67c1bad842a7b6fad88d0794d43bd25125b.

--- timeTravel/Assets/Scripts/Inventory/Artifact.cs
+++ timeTravel/Assets/Scripts/Inventory/Artifact.cs
@@ -2,10 +2,13 @@ using UnityEngine;
 
 public class Artifact : Item
 {
+    [SerializeField] private bool isCollected = false;
+
+    public bool IsCollected => isCollected;
+
     public override void Pickup(Inventory inventory)
     {
         if (inventory == null) return;
-<<<<<<< Updated upstream
 
         bool added = inventory.AddItem(this);
 
@@ -20,8 +23,5 @@ public class Artifact : Item
 
             gameObject.SetActive(false);
         }
-=======
-        inventory.AddItem(this);
->>>>>>> Stashed changes
     }
 }
